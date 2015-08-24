93f8c896d7a250fe3003d73e0ecad5e2f5a11a8b
keshav <keshav.sharma@xoriant.com>
2015-08-24 20:41:05 +0530

    syntax error fixed

--- manifests/website.pp
+++ manifests/website.pp
@@ -1,11 +1,11 @@
 class profile::website ( 
-	$name=hiera('profile::website::name'),
+	$webname=hiera('profile::website::name'),
 	$port=hiera('profile::website::port'),
 	$docroot=hiera('profile::website::docroot'),
 	$htmlname=hiera('profile::website::htmlname'),
     ) {
 
-websites::vhost { "$name":
+websites::vhost { "$webname":
  port => $port,
  docroot => $docroot,
  htmlname => $htmlname,
